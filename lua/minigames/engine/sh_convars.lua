12f3caa0c305233199dc49c0777da9d208cb3a8b
Alf Müller <4lf-mueller@gmx.de>
2020-04-07 18:00:04 +0200

    Added ttt2 ulx support

---
+++ lua/minigames/engine/sh_convars.lua
@@ -0,0 +1,66 @@
+if SERVER then
+    local ttt2_minigames = CreateConVar("ttt2_minigames", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED})
+    local ttt2_minigames_autostart = CreateConVar("ttt2_minigames_autostart", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_REPLICATED})
+
+    -- ConVar Replicating
+    hook.Add("TTTUlxInitCustomCVar", "TTT2MGInitRWCVar", function(name)
+        ULib.replicatedWritableCvar(ttt2_minigames:GetName(), "rep_" + ttt2_minigames:GetName(), ttt2_minigames:GetBool(), true, true, "xgui_gmsettings")
+        ULib.replicatedWritableCvar(ttt2_minigames_autostart:GetName(), "rep_" + ttt2_minigames_autostart:GetName(), ttt2_minigames_autostart:GetInt(), true, true, "xgui_gmsettings")
+    end)
+else
+    hook.Add("TTTUlxModifyAddonSettings", "TTT2MGModifySettings", function(name)
+        local pnl = xlib.makelistlayout{w = 415, h = 318, parent = xgui.null}
+
+        local clp = vgui.Create("DCollapsibleCategory", pnl)
+        clp:SetSize(390, 110)
+        clp:SetExpanded(1)
+        clp:SetLabel("Basic Settings")
+
+        local lst = vgui.Create("DPanelList", clp)
+        lst:SetPos(5, 25)
+        lst:SetSize(390, 110)
+        lst:SetSpacing(5)
+
+        lst:AddItem(xlib.makelabel{
+            x = 0,
+            y = 0,
+            w = 415,
+            wordwrap = true,
+            label = "Disabling TTT2 Minigames disables the functionality of any add-on that depends on it.",
+            parent = lst
+        })
+
+        lst:AddItem(xlib.makecheckbox{
+            label = "Enable TTT2 Minigames? (ttt2_minigames) (Def. 1)",
+            repconvar = "rep_ttt2_minigames",
+            parent = lst
+        })
+
+        lst:AddItem(xlib.makelabel{ -- empty line
+            x = 0,
+            y = 0,
+            w = 415,
+            wordwrap = true,
+            label = "",
+            parent = lst
+        })
+
+        lst:AddItem(xlib.makelabel{
+            x = 0,
+            y = 0,
+            w = 415,
+            wordwrap = true,
+            label = "Enabling TTT2 Minigames autostart will lead to the result that a random minigame is activated on every round start.",
+            parent = lst
+        })
+
+        lst:AddItem(xlib.makecheckbox{
+            label = "Enable TTT2 Minigames autostart? (ttt2_minigames_autostart) (Def. 1)",
+            repconvar = "rep_ttt2_minigames_autostart",
+            parent = lst
+        })
+
+        xgui.hookEvent("onProcessModules", nil, pnl.processModules)
+        xgui.addSubModule("TTT2 Minigames", pnl, nil, name)
+    end)
+end
--- lua/minigames/engine/sh_init.lua
+++ lua/minigames/engine/sh_init.lua
@@ -1,12 +1,13 @@
 if SERVER then
+	AddCSLuaFile("sh_convars.lua")
 	AddCSLuaFile("sh_vars.lua")
 	AddCSLuaFile("sh_networking.lua")
 	AddCSLuaFile("sh_functions.lua")
 
-	include("sv_convars.lua")
 	include("sv_hooks.lua")
 end
 
+include("sh_convars.lua")
 include("sh_vars.lua")
 include("sh_networking.lua")
 include("sh_functions.lua")
