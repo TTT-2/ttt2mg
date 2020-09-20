e234daf5dd32a511af23042e4f8e0c1a06b26f62
Alf Müller <4lf-mueller@gmx.de>
2020-09-20 17:12:51 +0200

    Added `MINIGAME:ShowActivationEPOP()` and fixed `active` state

--- lua/minigames/engine/sh_functions.lua
+++ lua/minigames/engine/sh_functions.lua
@@ -10,15 +10,6 @@ function ActivateMinigame(minigame)
 		net.Start("TTT2MGActivateMinigame")
 		net.WriteUInt(minigame.id, MINIGAMES_BITS)
 		net.Broadcast()
-	elseif GetConVar("ttt2_minigames_show_popup"):GetBool() and istable(minigame.lang) then -- show popup if a minigame is activated
-		EPOP:AddMessage({
-				text = LANG.TryTranslation("ttt2_minigames_" .. minigame.name .. "_name"),
-				color = COLOR_ORANGE
-			},
-			minigame.lang.desc and LANG.TryTranslation("ttt2_minigames_" .. minigame.name .. "_desc") or nil,
-			12,
-			true
-		)
 	end
 
 	hook.Run("TTT2MGActivate", minigame)
--- lua/minigames/minigames/minigame_base/shared.lua
+++ lua/minigames/minigames/minigame_base/shared.lua
@@ -5,9 +5,9 @@
 MINIGAME.author = "" -- author
 MINIGAME.contact = "" -- contact to the author
 
-MINIGAME.isAbstract = true -- abstract MINIGAME you can derive from 
+MINIGAME.isAbstract = true -- abstract MINIGAME you can derive from
 
-local active = false
+MINIGAME.m_bActive = false
 
 ---
 -- Activates the @{MINIGAME}
@@ -15,11 +15,16 @@ local active = false
 function MINIGAME:Activate()
 	self:OnActivation()
 
-	active = true
+	self.m_bActive = true
+
+	if CLIENT and GetConVar("ttt2_minigames_show_popup"):GetBool() then -- show popup if the @{MINIGAME} is activated
+		self:ShowActivationEPOP()
+	end
 end
 
 ---
 -- Called if the @{MINIGAME} activates
+-- @note At the time of the call, the @{MINIGAME} does not yet return `true` with @{MINIGAME:IsActive()}
 -- @hook
 -- @realm shared
 function MINIGAME:OnActivation()
@@ -32,11 +37,12 @@ end
 function MINIGAME:Deactivate()
 	self:OnDeactivation()
 
-	active = false
+	self.m_bActive = false
 end
 
 ---
 -- Called if the @{MINIGAME} deactivates
+-- @note At the time of the call, the @{MINIGAME} does not yet return `false` with @{MINIGAME:IsActive()}
 -- @hook
 -- @realm shared
 function MINIGAME:OnDeactivation()
@@ -44,10 +50,10 @@ function MINIGAME:OnDeactivation()
 end
 
 ---
--- Returns whether the current minigame is active
+-- Returns whether the current @{MINIGAME} is active
 -- @realm shared
 function MINIGAME:IsActive()
-	return active
+	return self.m_bActive
 end
 
 if SERVER then
@@ -57,5 +63,21 @@ if SERVER then
 	-- @realm server
 	function MINIGAME:IsSelectable()
 		return true
+	end
+else
+	---
+	-- This function is called as soon as the @{MINIGAME} is activated and should be used to display a EPOP message informing the player about the activation
+	-- @realm client
+	function MINIGAME:ShowActivationEPOP()
+		if not istable(self.lang) or not EPOP then return end
+
+		EPOP:AddMessage({
+				text = LANG.TryTranslation("ttt2_minigames_" .. self.name .. "_name"),
+				color = COLOR_ORANGE
+			},
+			self.lang.desc and LANG.TryTranslation("ttt2_minigames_" .. self.name .. "_desc") or nil,
+			12,
+			true
+		)
 	end
 end
