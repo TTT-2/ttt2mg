a7c8a5e2afe77aa9103fca2439046c802a62576b
Tim Goll <tim@timgoll.de>
2021-11-25 16:09:44 +0100

    fixed missing client convar

--- lua/minigames/engine/sh_vars.lua
+++ lua/minigames/engine/sh_vars.lua
@@ -1,5 +1,9 @@
-CreateConVar("ttt2_minigames", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
-CreateConVar("ttt2_minigames_autostart_random", "100", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
-CreateConVar("ttt2_minigames_autostart_rounds", "0", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+if SERVER then
+	CreateConVar("ttt2_minigames", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+	CreateConVar("ttt2_minigames_autostart_random", "100", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+	CreateConVar("ttt2_minigames_autostart_rounds", "0", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+else -- CLIENT
+	CreateConVar("ttt2_minigames_show_popup", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+end
 
 MINIGAMES_BITS = 10
